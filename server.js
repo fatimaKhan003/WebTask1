66be5cb7e7e9fdd956cdb433009167ddd788b2d9
Fatima Khan <163330776+fatimaKhan003@users.noreply.github.com>
2025-05-09 12:03:02 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ app.get('/',(req,res)=>
 {
     res.send('Hello World');
 });
-app.listen(5000,()=>
+const port = process.env.PORT || 3000;
+app.listen(port,()=>
 {
     console.log('Server is running on port 5000');
 })
